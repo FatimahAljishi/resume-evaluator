b517f797ffadffe3c11ab581bf3fcc0f76dbb48e
s2716746 <s2716746@ed.ac.uk>
2026-06-02 16:41:20 +0300

    validation

--- starter/app/routers/auth.py
+++ starter/app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from app.schemas import (
     RegisterRequest,
     LoginRequest,
@@ -6,10 +6,16 @@ from app.schemas import (
     TokenResponse,
 )
 
+
 router = APIRouter()
 
-@router.post("/register", response_model=UserResponse)
+users ={}
+
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def register(request: RegisterRequest):
+    if request.email in users:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    users[request.email] = request.password
     return UserResponse(email=request.email, role="user")
 
 @router.post("/login", response_model=TokenResponse)
--- starter/app/schemas.py
+++ starter/app/schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
 class RegisterRequest(BaseModel):
-    email: str
-    password: str
+    email: EmailStr
+    password: str = Field(min_length=8)
 
 class LoginRequest(BaseModel):
-    email: str
+    email: EmailStr
     password: str
 
 class UserResponse(BaseModel):
