0592b58226ecadcbf1d28c89bfef4df0dbb94df4
FATIMAH ALJISHI <s2716746@ed.ac.uk>
2026-07-07 17:45:12 +0300

    Update API base URL to use environment variable

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
 const client = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: {API_URL},
 });
 
 client.interceptors.request.use((config) => {
